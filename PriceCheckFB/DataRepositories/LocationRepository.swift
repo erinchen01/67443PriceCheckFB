dff23fe975305195e6be64d75a6fbc9976efd05e
Xinyi Chen <xinyic2@andrew.cmu.edu>
2024-10-01 15:43:02 -0400

    finished requried outcome

--- PriceCheckFB/DataRepositories/LocationRepository.swift
+++ PriceCheckFB/DataRepositories/LocationRepository.swift
@@ -6,14 +6,20 @@
 
 import Combine
 // import Firebase modules here
-
+import Firebase
+//import FirebaseCore
+import FirebaseFirestore
 
 
 
 class LocationRepository: ObservableObject {
   // Set up properties here
+
+  private let store = Firestore.firestore()
+  private let path = "location_scans"
+  @Published var locations: [Location] = []
   
-  
+  private var cancellables: Set<AnyCancellable> = []
   
   init() {
     get()
@@ -21,11 +27,17 @@ class LocationRepository: ObservableObject {
 
   func get() {
     // Complete this function
-    
-    
-    
-    
-    
-
+    store.collection(path)
+      .addSnapshotListener { querySnapshot, error in
+        if let error = error {
+          print("Error getting books: \(error.localizedDescription)")
+          return
+        }
+        
+        self.locations = querySnapshot?.documents.compactMap {
+          document in
+          try? document.data(as: Location.self)
+        } ?? []
+      }
   }
 }
